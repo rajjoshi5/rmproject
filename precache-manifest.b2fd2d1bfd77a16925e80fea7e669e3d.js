f06f3946de967aaf53753223464f2dc41312df5c
Raj Joshi <rajjoshipawan@gmail.com>
2024-10-13 19:04:42 +0100

    Updates

---
+++ precache-manifest.b2fd2d1bfd77a16925e80fea7e669e3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9335572f2505783cb6f3c59d66271743",
+    "url": "/rmproject/index.html"
+  },
+  {
+    "revision": "f4488c768b5276a6e50c",
+    "url": "/rmproject/static/css/main.cb6e5177.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/rmproject/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "f4488c768b5276a6e50c",
+    "url": "/rmproject/static/js/main.9de426ae.chunk.js"
+  },
+  {
+    "revision": "ae4102c89ae623aac517",
+    "url": "/rmproject/static/js/runtime~main.a72f9983.js"
+  }
+]);
